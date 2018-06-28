387b7b9d4e906a66baaf85efaf0b30bc6f15b167
MecaCho <1099833328@qq.com>
2018-06-29 00:01:19 +0800

    init comment

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "album_server/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"album_server/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
