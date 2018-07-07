911ce69a3e9dc184c213675fb59b4bf203dc12d3
MecaCho <1099833328@qq.com>
2018-07-07 23:44:12 +0800

    db model struct

--- models/db/base.go
+++ models/db/base.go
@@ -1,7 +1,7 @@
 package db
 
 import (
-	"beego/orm"
+	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -14,36 +14,4 @@ func init() {
 
 	orm.RunSyncdb("default", false, true)
 	ORM_ = orm.NewOrm()
-}
-
-
-type User struct {
-	IncID     int    `orm:"pk;column(inc_id)"json:"inc_id"`
-	Id        string `json:"id"`
-	Name      string `json:"name"`
-	Password  string `json:"password"`
-	CreateAt  string `json:"create_at"`
-	UpdatedAt string `json:"updated_at"`
-}
-
-type Album struct {
-	IncID      int64    `orm:"pk;column(inc_id)";json:"inc_id"`
-	Id         string   `json:"id"`
-	Name       string   `json:"name"`
-	UploadTime string   `json:"upload_time"`
-	Size       int      `json:"size"`
-	PhotoNum   int      `json:"photo_num"`
-	Photos     []*Photo `orm:"rel(m2m)";json:"photos"`
-	UserID     string   `json:"user_id"`
-}
-
-type Photo struct {
-	IncID      int    `orm:"pk;column(inc_id)";json:"inc_id"`
-	Id         string `json:"id"`
-	Name       string `json:"name"`
-	UploadTime string `json:"upload_time"`
-	Size       int    `json:"size"`
-	CreateAt   string `json:"create_at"`
-	UpdatedAt  string `json:"updated_at"`
-	Album      *Album `orm:"rel(fk)";json:"album"`
 }
--- models/db/model.go
+++ models/db/model.go
@@ -1,8 +1,32 @@
 package db
 
 type User struct {
-	Id int `json:"id"`
-	UUID string `json:"uuid"`
-	Name string `json:"name"`
-	Password string `json:"password"`
+	IncID     int    `orm:"pk;column(inc_id)"json:"inc_id"`
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	Password  string `json:"password"`
+	CreateAt  string `json:"create_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+type Album struct {
+	IncID      int64    `orm:"pk;column(inc_id)";json:"inc_id"`
+	Id         string   `json:"id"`
+	Name       string   `json:"name"`
+	UploadTime string   `json:"upload_time"`
+	Size       int      `json:"size"`
+	PhotoNum   int      `json:"photo_num"`
+	Photos     []*Photo `orm:"rel(m2m)";json:"photos"`
+	UserID     string   `json:"user_id"`
+}
+
+type Photo struct {
+	IncID      int    `orm:"pk;column(inc_id)";json:"inc_id"`
+	Id         string `json:"id"`
+	Name       string `json:"name"`
+	UploadTime string `json:"upload_time"`
+	Size       int    `json:"size"`
+	CreateAt   string `json:"create_at"`
+	UpdatedAt  string `json:"updated_at"`
+	Album      *Album `orm:"rel(fk)";json:"album"`
 }
