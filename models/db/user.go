791736fab39dc337a1661b970e5079fe81caff10
MecaCho <1099833328@qq.com>
2018-07-28 23:29:12 +0800

    db user

--- models/db/user.go
+++ models/db/user.go
@@ -13,17 +13,29 @@ func InsertUser(user User) error {
 	user.IncID = maxIncID + 1
 	user.UpdatedAt = time.Now().String()
 
-	fmt.Printf("Insert Photo : %#v", user)
+	fmt.Printf("Insert User : %#v", user)
 	id, err := ORM_.Insert(&user)
 	if err != nil {
-		fmt.Printf("Insert Photo (%q) Error (%d) : (%q)\n", user.Id, id, err)
+		fmt.Printf("Insert User (%q) Error (%d) : (%q)\n", user.Id, id, err)
 		return err
 	}
-	fmt.Printf("Insert Photo (%q) Success : %d", user.Id, id)
+	fmt.Printf("Insert User (%q) Success : %d", user.Id, id)
 	return nil
 }
 
-func GetUser(userName string) (string, error) {
+func GetUser(userName string) (User, error) {
+	var user User
+	qs := ORM_.QueryTable(user)
+	num, err := qs.Filter("Name", userName).All(&user)
+	if err != nil {
+		fmt.Printf("Get User (%s) Error : (%q) , %s \n", userName, err, num)
+		return user, err
+	}
+	fmt.Printf("Get PWD (%q) Success: %#v , %s", userName, user, num)
+	return user, nil
+}
+
+func GetUserPWD(userName string) (string, error) {
 	var user User
 	qs := ORM_.QueryTable(user)
 	num, err := qs.Filter("Name", userName).All(&user)
